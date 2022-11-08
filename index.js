6abf7043ecba2aec2ce23dbcef270b7d0da904f7
Ezequiel Mahafud <emahafud@provincianet.com.ar>
2022-11-08 12:34:25 -0300

    Test API codigos de descuento

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const discountCodesRouter = require('./routes/discountCodesRouter')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(logger)
 
 app.get('/', (req, res) => {
