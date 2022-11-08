a679ef24c8a2717742f7f266101828a8b1deaf8f
Ezequiel Mahafud <emahafud@provincianet.com.ar>
2022-11-08 12:17:41 -0300

    Test API codigos de descuento

--- routes/discountCodesRouter.js
+++ routes/discountCodesRouter.js
@@ -38,7 +38,7 @@ discountCodesRouter.get('/code/:code', (req, res, next) => {
 });
 
 discountCodesRouter.post('/', (req, res, next) => {
-    res.json(req).status(200).send();
+    res.json(req.json()).status(200).send();
     /*const { code, amount, used, due_date, created_by } = req.body;
     const newDiscountCode = new DiscountCode({ code, amount, used, due_date, created_by });
         
