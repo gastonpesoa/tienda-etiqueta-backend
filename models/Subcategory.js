c37307d6aa18ef0942546b7a055d0f3087eb4a24
Ezequiel Mahafud <emahafud@provincianet.com.ar>
2022-12-06 15:06:25 -0300

    Agregado model de subcategorias

---
+++ models/Subcategory.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Category = require('./Category')
+const { model, Schema } = mongoose
+
+const SubcategorySchema = new Schema({
+    category: { type: Category.schema, required: true },
+    name: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    }
+})
+
+SubcategorySchema.set('toJSON', {
+    transform: ((document, subcategoryToJSON) => {
+        subcategoryToJSON.id = subcategoryToJSON._id.toString()
+        delete subcategoryToJSON._id
+        delete subcategoryToJSON.__v
+    })
+})
+
+const Subcategory = model('Subcategory', SubcategorySchema)
+
+module.exports = Subcategory
